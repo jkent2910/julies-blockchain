739169b6b2ef21f13afc04af34429ae35c6a6c3d
Julie Kent <jkent2910@gmail.com>
2019-04-16 19:50:10 -0700

    fix buggy

--- client/src/components/TransactionPool.js
+++ client/src/components/TransactionPool.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Transaction from './Transaction';
-import history from '../history';
 import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
+import { Link } from 'react-router-dom';
+import history from '../history';
 
-const POLL_INTERVAL_MS = 10000;
+const POLL_INERVAL_MS = 10000;
 
 class TransactionPool extends Component {
     state = { transactionPoolMap: {} };
@@ -25,14 +25,14 @@ class TransactionPool extends Component {
                     alert('The mine-transactions block request did not complete.');
                 }
             });
-    };
+    }
 
     componentDidMount() {
         this.fetchTransactionPoolMap();
 
         this.fetchPoolMapInterval = setInterval(
             () => this.fetchTransactionPoolMap(),
-            POLL_INTERVAL_MS
+            POLL_INERVAL_MS
         );
     }
 
@@ -41,11 +41,9 @@ class TransactionPool extends Component {
     }
 
     render() {
-        return(
+        return (
             <div className='TransactionPool'>
-                <div>
-                    <Link to='/'>Home</Link>
-                </div>
+                <div><Link to='/'>Home</Link></div>
                 <h3>Transaction Pool</h3>
                 {
                     Object.values(this.state.transactionPoolMap).map(transaction => {
@@ -58,7 +56,12 @@ class TransactionPool extends Component {
                     })
                 }
                 <hr />
-                <Button bsStyle='danger' onClick={this.fetchMineTransactions}>Mine the Transactions</Button>
+                <Button
+                    bsStyle="danger"
+                    onClick={this.fetchMineTransactions}
+                >
+                    Mine the Transactions
+                </Button>
             </div>
         )
     }
